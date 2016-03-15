f32bfa0ddcc41d5f2929043c3c6204eafdbaa272
Masayuki Itabashi <rada.bashishi@gmail.com>
2016-03-15 19:25:11 +0900

    parse path

--- etc/td-agent/plugin/parameter_parser.rb
+++ etc/td-agent/plugin/parameter_parser.rb
@@ -3,16 +3,31 @@
 
 require 'json'
 require 'msgpack'
+require 'uri'
 require 'date'
 
-fdate = '%Y/%m/%d %H:%M:%S'
+str = "{\"path\":\"http://example.com/abc?param1=xxx\"}"
 
 while str = STDIN.gets
     str.chomp!
     begin
         s = JSON.parse(str)
-        s.store("FlunetdTime", DateTime.now.strftime(fdate))
-    rescue
+        unless s["path"].nil?
+            uri = URI(s["path"])
+            query = uri.query
+            unless query.empty?
+                q = query.split("&")
+                q.each do |param|
+                    p = param.split("=")
+                    if p.length > 1 then
+                      s["path." + p[0].to_s] = p[1]
+                    else
+                      s["path." + p[0].to_s] = ""
+                    end
+                end
+            end
+        end
+    rescue => ex
         s = str 
     end
     print MessagePack.pack(s)
