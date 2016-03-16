da02525d1d0f73e85ae50ebd269f26a07f5cf7cf
Masayuki Itabashi <rada.bashishi@gmail.com>
2016-03-16 12:40:19 +0900

    JSON keys modified array

--- etc/td-agent/exec/parameter_parser.rb
+++ etc/td-agent/exec/parameter_parser.rb
@@ -4,23 +4,27 @@
 require 'json'
 require 'msgpack'
 require 'uri'
-require 'date'
+
+keys = ["path", "referer", "url"]
 
 while str = STDIN.gets
     str.chomp!
     begin
         s = JSON.parse(str)
-        unless s["path"].nil?
-            uri = URI(s["path"])
-            query = uri.query
-            unless query.empty?
-                q = query.split("&")
-                q.each do |param|
-                    p = param.split("=")
-                    if p.length > 1 then
-                      s["path." + p[0].to_s] = p[1]
-                    else
-                      s["path." + p[0].to_s] = ""
+        keys.each do |key|
+            unless s[key].nil?
+                uri = URI(s[key])
+                query = uri.query
+                unless query.empty?
+                    q = query.split("&")
+                    q.each do |param|
+                        p = param.split("=")
+                        k = key + "." + p[0].to_s
+                        if p.length > 1 then
+                          s[k] = p[1]
+                        else
+                          s[k] = ""
+                        end
                     end
                 end
             end
